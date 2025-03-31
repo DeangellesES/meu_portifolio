b1d3d09eaf1fa6ed2f1c1673ee8a0ebfded17f3b
Felipe <FelipeDeangelles@hotmail.com>
2025-03-31 02:14:47 -0300

    adicionando swiper

--- assets/js/script.js
+++ assets/js/script.js
@@ -73,49 +73,25 @@ function MENUCLICK() {
 
     function clicou() {
         nav.classList.toggle("ativo")
-    } 
-} 
+    }
+}
 
 MENUCLICK()
 
-// FUNCIONALIDADE SWIPER
-var swiper = new Swiper(".popular-content", {
-    slidesPerView: 1,
-    spaceBetween: 10,
-    autoplay: {
-        delay: 5500,
-        disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 4,
+    // autoplay: {
+    //     delay: 5500,
+    //     disableOnInteraction: false,
+    // },
 
-    },
+    // Optional parameters
+    loop: true,
+
+    // Navigation arrows
     navigation: {
-     nextEl: ".swiper-button-next",
-     prevEl: ".swiper-button-prev",
-    },
-    breakpoints:{
-        280: {
-            slidesPerView: 1,
-            spaceBetween: 10,
-        },
-        320: {
-            slidesPerView: 2,
-            spaceBetween: 10,
-        },
-        510: {
-            slidesPerView: 2,
-            spaceBetween: 10,
-        },
-        758: {
-            slidesPerView: 3,
-            spaceBetween: 15,
-        },
-        900: {
-            slidesPerView: 4,
-            spaceBetween: 20,
-        },
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
 
 });
